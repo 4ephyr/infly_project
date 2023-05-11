70c9d19d90e49b6adaadf1f9f1022ea63951f239
Mohammed Sahal <technicalsahal25@gmail.com>
2023-05-11 21:47:38 +0530

    First section completed

--- js/main.js
+++ js/main.js
@@ -2,9 +2,11 @@ let link1 = document.getElementById('svg1')
 let svg1 = document.getElementById('dropicon')
 let boxes = document.getElementsByClassName('incheight')
 let hello = document.getElementsByClassName('test')
+let items = document.getElementsByClassName('item1')
+let itempic = document.getElementsByClassName('itempic')
 let x = boxes.length
-let y = hello.length
-console.log(y)
+let y = items.length
+// console.log(z)
 link1.addEventListener('mouseover',()=>{
     svg1.classList.add('svghover')
 })
@@ -14,16 +16,32 @@ link1.addEventListener('mouseout',()=>{
 
 for(let i = 0;i < x;i++){
     boxes[i].addEventListener('mouseover',()=>{
-        for(let j = 0;j<y;j++){
             hello[i].classList.add('height')
-        }
     })
 }
 
 for(let i = 0;i < x;i++){
     boxes[i].addEventListener('mouseout',()=>{
-        for(let j = 0;j<y;j++){
             hello[i].classList.remove('height')
-        }
     })
 }
+
+for(let i = 0;i < x;i++){
+    boxes[i].addEventListener('mouseout',()=>{
+            hello[i].classList.remove('box-shadow')
+    })
+}
+
+for(let j = 0;j < y;j++){
+    items[j].addEventListener('mouseover',()=>{
+            itempic[j].classList.add('box-shadow')
+    })
+}
+
+for(let j = 0;j < y;j++){
+    items[j].addEventListener('mouseout',()=>{
+            itempic[j].classList.remove('box-shadow')
+    })
+}
+
+
