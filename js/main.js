e31bf957e2abc9b424a33ae6268539e7c7bb8258
Mohammed Sahal <technicalsahal25@gmail.com>
2023-05-21 22:08:36 +0530

    Pages are now responsive

--- js/main.js
+++ js/main.js
@@ -5,47 +5,49 @@ const hello = document.getElementsByClassName('test');
 const items = document.getElementsByClassName('item1');
 const itempic = document.getElementsByClassName('itempic');
 const hiddenbox = document.getElementById('hiddenbox');
-const sec3 = document.getElementsByClassName('sec3full')
-const feedback = document.getElementById('feedbackbtn')
-const products = document.getElementById('products')
-const popup = document.getElementById('popup')
+const sec3 = document.getElementsByClassName('sec3full');
+const feedback = document.getElementById('feedbackbtn');
+const products = document.getElementById('products');
+const aboutus = document.querySelector('.aboutus')
+const feed = document.querySelector('.feed')
+const popup = document.getElementById('popup');
 const overlay = document.createElement('div');
 
 const x = boxes.length;
 const y = items.length;
 
-link1.addEventListener('mouseover',()=>{
+link1.addEventListener('mouseover', () => {
   svg1.classList.add('svghover');
 });
 
-link1.addEventListener('mouseout',()=>{
+link1.addEventListener('mouseout', () => {
   svg1.classList.remove('svghover');
 });
 
-const handleMouseOver = (index)=>{
+const handleMouseOver = (index) => {
   hello[index].classList.add('height');
 };
 
-const handleMouseOut = (index)=>{
+const handleMouseOut = (index) => {
   hello[index].classList.remove('height');
 };
 
 for (let i = 0; i < x; i++) {
-  boxes[i].addEventListener('mouseover',()=>{
+  boxes[i].addEventListener('mouseover', () => {
     handleMouseOver(i);
   });
 
-  boxes[i].addEventListener('mouseout',()=>{
+  boxes[i].addEventListener('mouseout', () => {
     handleMouseOut(i);
   });
 }
 
 for (let j = 0; j < y; j++) {
-  items[j].addEventListener('mouseover',()=>{
+  items[j].addEventListener('mouseover', () => {
     itempic[j].classList.add('box-shadow');
   });
 
-  items[j].addEventListener('mouseout',()=>{
+  items[j].addEventListener('mouseout', () => {
     itempic[j].classList.remove('box-shadow');
   });
 }
@@ -53,10 +55,14 @@ for (let j = 0; j < y; j++) {
 for (let j = 0; j < y; j++) {
   items[j].addEventListener('click', (event) => {
     event.stopPropagation(); // Stop the click event from propagating to the document
-    console.log(hiddenbox);
-    hiddenbox.classList.toggle('width');
+  if (window.innerWidth<930) {
+      hiddenbox.classList.add('w-100') 
+  }
+  else{
+  hiddenbox.classList.toggle('width');
+  }
   });
-} 
+}
 
 document.addEventListener('click', (event) => {
   const clickedElement = event.target;
@@ -74,23 +80,35 @@ document.addEventListener('click', (event) => {
   }
 });
 
-function hideProducts(event){
-  products.classList.toggle('opacity')
+function hideProducts(event) {
+  products.classList.toggle('opacity');
   event.stopPropagation();
 }
 
 function closeNav() {
-  document.getElementById('hiddenbox').classList.remove('width');
+  if(hiddenbox.classList.contains('w-100')){
+    hiddenbox.classList.remove('w-100')
+  }
+  else{
+    document.getElementById('hiddenbox').classList.remove('width');
+  }
 }
-function closeExp(){
-  popup.classList.remove('click')
+
+function closeExp() {
+  popup.classList.remove('click');
   document.body.removeChild(overlay);
 }
+
 feedback.addEventListener('click', (event) => {
   event.stopPropagation();
   popup.classList.toggle('click');
   document.body.appendChild(overlay);
 });
+document.querySelector('#rate').addEventListener('click',()=>{
+  event.stopPropagation();
+  popup.classList.toggle('click');
+  document.body.appendChild(overlay);
+})
 
 popup.addEventListener('click', (event) => {
   event.stopPropagation();
@@ -102,3 +120,9 @@ overlay.addEventListener('click', () => {
   popup.classList.remove('click');
   document.body.removeChild(overlay);
 });
+
+if(window.innerWidth < 930){
+  document.querySelector('.sec2main').insertBefore(aboutus , feed)
+  // document.querySelector('.sec2main').replaceChild(feed , aboutus)
+  document.querySelector('.newsh1').innerHTML = 'Stay Up-to-Date with Our Monthly Newsletter';
+}
